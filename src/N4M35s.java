bbcdd63b9b8a4d1fa2c99c6626c3b18696e07b99
hannah <hannah@dalton.org>
2017-12-11 11:32:53 -0500

    added spacify

--- src/N4M35s.java
+++ src/N4M35s.java
@@ -22,53 +22,42 @@ public class N4M35s {
         //Abrahams, Grace '20
 
 
-
         //Allina, Nicole '20
-
 
 
         //Bascobert, Paul (Paul) '19
 
 
-
         //Cahill, Phoebe '20
-
 
 
         //Firestone, Nicholas '20
 
 
-
         //Foley, Timothy (Teddy) '20
-
 
 
         //Jamee, Mehrab '20
 
 
-
         //Jiang, Claire '20
-
 
 
         //Katz, Liza '20
 
 
-
         //Levin, Hannah '20
-
-
+        //hannah's spacify display
+        String space = spacify(name);
+        frame.drawText("spacify: " + space, 120, 160, 20, 0, ColorLatte.MediumPurple);
 
         //Miller, Marshall '20
-
 
 
         //Morse, Madeline (Maddie) '20
 
 
-
         //Radomisli, Julia '20
-
 
 
         //Schoeman, Isabel '20
@@ -77,6 +66,7 @@ public class N4M35s {
         //paint:
         frame.paint();
     }
+
 
 
     //charlie's initials function:
@@ -130,6 +120,18 @@ public class N4M35s {
 
 
     //Levin, Hannah '20
+    static String spacify(String name) {
+        String spacify = "";
+
+        //make array of letters
+        char[] letters = new char[name.length()];
+        for (int i = 0; i < letters.length; i++) {
+            letters[i] = name.charAt(i);
+            spacify = spacify + letters[i] + " ";
+        }
+
+        return spacify;
+    }
 
 
 

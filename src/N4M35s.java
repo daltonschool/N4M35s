7f9c6d5dbc3c0a63295d47a8ec169b6a1ede7e19
julia <julia@dalton.org>
2017-12-11 11:23:23 -0500

    added ship names

--- src/N4M35s.java
+++ src/N4M35s.java
@@ -12,6 +12,10 @@ public class N4M35s {
         frame.println("please enter your full name");
         name = frame.nextLine().toString();
 
+        //take a second name:
+        frame.println("please enter another full name");
+        String name2 = frame.nextLine().toString();
+
         //draw the output:
         frame.drawText(name, 100, 100, 30, 0, ColorLatte.Aquamarine);
 
@@ -22,53 +26,42 @@ public class N4M35s {
         //Abrahams, Grace '20
 
 
-
         //Allina, Nicole '20
-
 
 
         //Bascobert, Paul (Paul) '19
 
 
-
         //Cahill, Phoebe '20
-
 
 
         //Firestone, Nicholas '20
 
 
-
         //Foley, Timothy (Teddy) '20
-
 
 
         //Jamee, Mehrab '20
 
 
-
         //Jiang, Claire '20
-
 
 
         //Katz, Liza '20
 
 
-
         //Levin, Hannah '20
-
 
 
         //Miller, Marshall '20
 
 
-
         //Morse, Madeline (Maddie) '20
 
 
-
         //Radomisli, Julia '20
-
+        String ship = shipname(name, name2);
+        frame.drawText("ship name: " + ship, 300, 200, 20, 0, ColorLatte.Violet);
 
 
         //Schoeman, Isabel '20
@@ -84,11 +77,11 @@ public class N4M35s {
         String initials;
 
         //first initial:
-        initials = name.charAt(0) +"";
+        initials = name.charAt(0) + "";
 
         //second initial:
         int space = name.indexOf(" ");
-        initials += name.charAt(space+1);
+        initials += name.charAt(space + 1);
 
         return initials;
     }
@@ -96,57 +89,59 @@ public class N4M35s {
     //Abrahams, Grace '20
 
 
-
     //Allina, Nicole '20
-
 
 
     //Bascobert, Paul (Paul) '19
 
 
-
     //Cahill, Phoebe '20
-
 
 
     //Firestone, Nicholas '20
 
 
-
     //Foley, Timothy (Teddy) '20
-
 
 
     //Jamee, Mehrab '20
 
 
-
     //Jiang, Claire '20
-
 
 
     //Katz, Liza '20
 
 
-
     //Levin, Hannah '20
-
 
 
     //Miller, Marshall '20
 
 
-
     //Morse, Madeline (Maddie) '20
 
 
-
     //Radomisli, Julia '20
+    static String shipname(String name, String name2) {
+        String shipname;
 
 
+        //first three letters:
+        shipname = name.charAt(0) + "";
+        shipname += name.charAt(1) + "";
+        shipname += name.charAt(2) + "";
 
-    //Schoeman, Isabel '20
+        //next three letters:
+        shipname += name2.charAt(0) + "";
+        shipname += name2.charAt(1) + "";
+        shipname += name2.charAt(2) + "";
+
+        return shipname;
 
 
+        //Schoeman, Isabel '20
 
+
+    }
 }

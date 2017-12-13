6bb0145dacc0b48ba19750c399b77e657cfe8606
isabel <c20is@dalton.org>
2017-12-13 09:09:54 -0500

    fixed some layout

--- src/N4M35s.java
+++ src/N4M35s.java
@@ -43,7 +43,7 @@ public class N4M35s {
 
         //Firestone, Nicholas '20
         String oodled = oodlifey(name);
-        frame.drawText("oodled: " + oodled, 100, 230, 20, 0, ColorLatte.BlueViolet);
+        frame.drawText("oodled: " + oodled, 100, 260, 20, 0, ColorLatte.BlueViolet);
 
 
         //Foley, Timothy (Teddy) '20
